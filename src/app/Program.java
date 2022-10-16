b4608d9459ed9060395af981dd76ec35283e75cf
Pedro Ruiz Marrara <pedrormarrara@gmail.com>
2022-10-16 11:58:04 +0100

    file getMethods

--- src/app/Program.java
+++ src/app/Program.java
@@ -8,27 +8,18 @@ public class Program {
     public static void main(String[] args) {
 
        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a folder path: ");
+
+        System.out.println("Enter a file path: ");
         String strPath = sc.nextLine();
 
         File path = new File(strPath);
 
-        File[] folders = path.listFiles(File::isDirectory);
-        System.out.println("Folders: " );
-        for (File folder : folders){
-            System.out.println(folder);
-        }
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
 
-        File [] files = path.listFiles(File :: isFile);
-        System.out.println("Files: ");
-        for (File file : files){
-            System.out.println(file);
-        }
 
-        boolean success = new File(strPath + "\\subdir").mkdir();
-        System.out.println("Directory successfully created: " + success);
-
-        sc.close();
+       sc.close();
 
     }
 }
